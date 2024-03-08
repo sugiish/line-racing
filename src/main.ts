f272d9d649a419772681cd6383136333cc09d965
k-seta <swordh16@gmail.com>
2024-03-09 03:42:44 +0900

    Phaser の tutorial 画面が表示されるように修正

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from 'phaser';
+import MyScene from "./scenes/MyScene";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>import Phaser from "phaser";
-    import MyScene from "./scenes/MyScene";
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'app',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 200 },
+    },
+  },
+  scene: MyScene,
+};
 
-    const config: Phaser.Types.Core.GameConfig = {
-      type: Phaser.AUTO,
-      width: 800,
-      height: 600,
-      physics: {
-        default: "arcade",
-        arcade: {
-          gravity: { y: 200 },
-        },
-      },
-      scene: MyScene,
-    };
-    new Phaser.Game(config);
-
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+new Phaser.Game(config);
