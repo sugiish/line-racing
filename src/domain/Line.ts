2bf51a5ea37a664269b0cd19ae5d1a7ecad1d666
k-seta <swordh16@gmail.com>
2024-03-09 16:55:37 +0900

    Line class を作成

---
+++ src/domain/Line.ts
@@ -0,0 +1,40 @@
+import Phaser from "phaser";
+import { Coordinate } from "./Coordinate"
+
+export default class Line {
+
+  private static MAX_DRAW_STEP = 1000;
+
+  graphics: Phaser.GameObjects.Graphics;
+  src: Coordinate;
+  dest: Coordinate;
+  isCompletedDrawing: boolean = false;
+  step: number = 0;
+
+  constructor(scene: Phaser.Scene, src: Coordinate, dest: Coordinate, color: number) {
+    this.graphics = scene.add.graphics({
+      lineStyle: {
+        width: 5,
+        color: color,
+      }
+    });
+    this.src = src;
+    this.dest = dest;
+  }
+
+  draw() {
+    if (this.isCompletedDrawing) {
+      return;
+    }
+
+    const x = this.src.x + (this.dest.x - this.src.x) * this.step / Line.MAX_DRAW_STEP;
+    const y = this.src.y + (this.dest.y - this.src.y) * this.step / Line.MAX_DRAW_STEP;
+    this.graphics.lineBetween(this.src.x, this.src.y, x, y);
+
+    this.step++;
+
+    if (this.step == Line.MAX_DRAW_STEP) {
+      this.isCompletedDrawing = true;
+    }
+  }
+}
--- src/scenes/MyScene.ts
+++ src/scenes/MyScene.ts
@@ -1,10 +1,13 @@
 import Phaser from "phaser";
+import Line from "../domain/Line";
 
 export default class MyScene extends Phaser.Scene {
 
   // = 0: 空のマス
   // > 0: 任意のプレイヤーが通過済みのマス
   board: number[][] = new Array(20).fill(new Array(30).fill(0));
+
+  line: Line | null = null;
 
   constructor() {
     super("main");
@@ -32,5 +35,11 @@ export default class MyScene extends Phaser.Scene {
         area.fillRect(75 + width * x, 75 + height * y, width, height);
       }
     }
+
+    this.line = new Line(this, { x: 0, y: 0 }, { x: 500, y: 500 }, 0xff0000);
+  }
+
+  update() {
+    this.line?.draw();
   }
 }
