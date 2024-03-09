31e4e418166fb883cda392b777cc0ee6356d2f9f
k-seta <swordh16@gmail.com>
2024-03-09 17:00:48 +0900

    定数定義して外出し

--- src/scenes/MyScene.ts
+++ src/scenes/MyScene.ts
@@ -1,6 +1,9 @@
 import Phaser from "phaser";
 import Line from "../domain/Line";
 
+const BOARD_OFFSET = { x: 75, y: 75 };
+const SQUARE_SIZE = { width: 30, height: 30 };
+
 export default class MyScene extends Phaser.Scene {
 
   // = 0: 空のマス
@@ -17,7 +20,7 @@ export default class MyScene extends Phaser.Scene {
 
   create() {
     // 盤面描画
-    const area = this.add.graphics({
+    const square = this.add.graphics({
       lineStyle: {
         width: 2,
         color: 0xf0f0f0,
@@ -29,10 +32,8 @@ export default class MyScene extends Phaser.Scene {
 
     for (let y = 0; y < this.board.length; y++) {
       for (let x = 0; x < this.board[y].length; x++) {
-        let width = 35;
-        let height = 35;
-        area.strokeRect(75 + width * x, 75 + height * y, width, height);
-        area.fillRect(75 + width * x, 75 + height * y, width, height);
+        square.strokeRect(BOARD_OFFSET.x + SQUARE_SIZE.width * x, BOARD_OFFSET.y + SQUARE_SIZE.height * y, SQUARE_SIZE.width, SQUARE_SIZE.height);
+        square.fillRect(BOARD_OFFSET.x + SQUARE_SIZE.width * x, BOARD_OFFSET.y + SQUARE_SIZE.height * y, SQUARE_SIZE.width, SQUARE_SIZE.height);
       }
     }
 
