037755fcecbc825a7c1368c340c4ad2c5a0278f9
sugiish <sugiish.me@gmail.com>
2024-03-16 19:40:53 +0900

    Add ruff

--- docker-resources/player1/main.py
+++ docker-resources/player1/main.py
@@ -1,22 +1,24 @@
-from enum import Enum
-from fastapi import FastAPI
 import dataclasses
+from enum import Enum
+
 import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @dataclasses.dataclass
 class Coordinate:
-  id: int
-  x: int
-  y: int
+    id: int
+    x: int
+    y: int
 
 
 @dataclasses.dataclass
 class RequestBody:
-  id: int
-  heads: list[Coordinate]
-  board: list[list[int]] # 左上原点。x 軸右向き、y 軸左向き。(x, y) = board[y][x]
+    id: int
+    heads: list[Coordinate]
+    board: list[list[int]]  # 左上原点。x 軸右向き、y 軸左向き。(x, y) = board[y][x]
 
 
 class EnumOps(Enum):
@@ -33,37 +35,43 @@ class EnumOps(Enum):
 
 @dataclasses.dataclass
 class ResponseModel:
-  ops: EnumOps
+    ops: EnumOps
 
 
 @app.post("/v1/next")
 def create_user(body: RequestBody):
+    # TODO: ここからを独自のアルゴリズムに修正する(5秒以内にレスポンスを返せるようにすること)
 
-  # TODO: ここからを独自のアルゴリズムに修正する(5秒以内にレスポンスを返せるようにすること)
+    head = next(filter(lambda x: x.id == body.id, body.heads), Coordinate(-1, -1, -1))
 
-  head = next(filter(lambda x: x.id == body.id, body.heads), Coordinate(-1, -1, -1))
+    for ops in EnumOps:
+        if ops == EnumOps.up:
+            dest = Coordinate(body.id, head.x, head.y - 1)
+        elif ops == EnumOps.right:
+            dest = Coordinate(body.id, head.x + 1, head.y)
+        elif ops == EnumOps.left:
+            dest = Coordinate(body.id, head.x - 1, head.y)
+        elif ops == EnumOps.down:
+            dest = Coordinate(body.id, head.x, head.y + 1)
 
-  for ops in EnumOps:
-      if(ops == EnumOps.up):
-        dest = Coordinate(body.id, head.x, head.y - 1)
-      elif(ops == EnumOps.right):
-        dest = Coordinate(body.id, head.x + 1, head.y)
-      elif(ops == EnumOps.left):
-        dest = Coordinate(body.id, head.x - 1, head.y)
-      elif(ops == EnumOps.down):
-        dest = Coordinate(body.id, head.x, head.y + 1)
+        if (
+            dest.x >= 0
+            and dest.y >= 0
+            and dest.x < len(body.board[0])
+            and dest.y < len(body.board)
+            and body.board[dest.y][dest.x] == 0
+        ):
+            return ResponseModel(ops)
 
-      if(dest.x >= 0 and dest.y >= 0 and dest.x < len(body.board[0]) and dest.y < len(body.board) and body.board[dest.y][dest.x] == 0):
-        return ResponseModel(ops)
+    # TODO: ここまでを独自のアルゴリズムに修正する
 
-  # TODO: ここまでを独自のアルゴリズムに修正する
-
-  return ResponseModel(EnumOps.checkmated)
+    return ResponseModel(EnumOps.checkmated)
 
 
 @app.get("/health")
 async def health():
     return {"status": "up"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
